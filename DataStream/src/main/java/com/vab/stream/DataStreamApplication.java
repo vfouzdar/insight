3a1aa0461b444751990f7bed83fdd88bdb9bb9aa
Vaibhav F <vaibhav.fouzdar@ericsson.com>
2020-01-09 16:10:21 -0600

    initiated new project to stream data to/from message queues

---
+++ DataStream/src/main/java/com/vab/stream/DataStreamApplication.java
@@ -0,0 +1,13 @@
+package com.vab.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStreamApplication.class, args);
+	}
+
+}
