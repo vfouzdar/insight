c04a64ab259534259dd2a9db7d46656c75157669
Vaibhav F <vaibhav.fouzdar@ericsson.com>
2020-01-23 16:53:39 -0600

    Relays message from datastream

---
+++ Relay/src/main/java/com/vab/relay/DataRelayApplication.java
@@ -0,0 +1,13 @@
+package com.vab.relay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRelayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataRelayApplication.class, args);
+	}
+
+}
