bb257b625f13677f04496a1424fac5b4c211dce9
VFDR <6243492+vfouzdar@users.noreply.github.com>
2020-01-31 15:56:29 -0600

    reverse with dep

---
+++ ReverseWithDep.java
@@ -0,0 +1,23 @@
+package io.kubeless;
+
+import io.kubeless.Event;
+import io.kubeless.Context;
+import org.apache.log4j.Logger;
+
+
+public class ReverseWithDep {
+Logger logger = Logger.getLogger(ReverseService.class.getName());
+    
+    public String reverseStr(Event event, Context context) {
+       System.out.println("Event Data--> " + event.Data);
+       logger.info("From Event Data--> " + event.Data);
+	
+        try {
+            System.out.println("Sleeping for 2s");
+			Thread.sleep(2000);
+		} catch (InterruptedException e1) {
+			System.out.println(e1);
+		}
+       return "wolleH";
+    }
+}
