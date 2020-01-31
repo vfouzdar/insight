cbf561ebfae0a60abdc13bf334e94424eebf0dc5
VFDR <6243492+vfouzdar@users.noreply.github.com>
2020-01-31 13:18:08 -0600

    request

--- ReverseService.java
+++ ReverseService.java
@@ -8,6 +8,7 @@ public class ReverseService {
     
     public String reverseStr(Event event, Context context) {
        System.out.println("Event Data--> " + event.Data);
+	    System.out.println("Request Data--> " + event.extension.request);
         try {
             System.out.println("Sleeping for 2s");
 			Thread.sleep(2000);
