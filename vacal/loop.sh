e790c70cb96a98c78daa1a208cccbf73767fa915
Fouzdar, Vaibhav (vf846v) <vf846v@us.att.com>
2020-09-02 07:03:02 -0500

    Hit vacal

---
+++ vacal/loop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for i in {1..999};
+do
+        curl -s "https://tinyurl.com/y5sf43af" > 1.html
+        sleep 2
+        curl "https://tinyurl.com/yxs4ybdn" > 2.html
+        sleep 2
+        curl --silent --output /dev/null --show-error --fail "https://vachienterprisesraipur.business.site/"  > 3.html
+done
+
+
