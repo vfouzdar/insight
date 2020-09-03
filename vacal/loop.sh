7901b626dbc95e251764f4f50a0413384fb1bd35
VFDR <6243492+vfouzdar@users.noreply.github.com>
2020-09-03 08:11:09 -0500

    Update loop.sh
    Added justdial search

--- vacal/loop.sh
+++ vacal/loop.sh
@@ -6,6 +6,9 @@ do
         curl "https://tinyurl.com/yxs4ybdn" > 2.html
         sleep 2
         curl --silent --output /dev/null --show-error --fail "https://vachienterprisesraipur.business.site/"  > 3.html
+        sleep 2
+        curl "https://www.justdial.com/Raipur-Chhattisgarh/search?q=vachi-aluminium" > 4.html
+        sleep 1
 done
 
 
